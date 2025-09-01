c2b1bdc07cf206d6379c35097d45e68c210cdb5c
Martin Teye <martinteye93@gmail.com>
2025-09-01 16:45:38 +0000

    Connect finance routes in project urls.py

--- smartbooks_api/finance/views.py
+++ smartbooks_api/finance/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from .models import Income, Expense
+from rest_framework import viewsets, permissions
 from .serializers import IncomeSerializer, ExpenseSerializer
+from .models import Account, Category
+from .serializers import AccountSerializer, CategorySerializer
 
 class IncomeViewSet(viewsets.ModelViewSet):
     queryset = Income.objects.all()
@@ -9,3 +12,24 @@ class IncomeViewSet(viewsets.ModelViewSet):
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
+
+class AccountViewSet(viewsets.ModelViewSet):
+    serializer_class = AccountSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Return only accounts belonging to logged-in user
+        return Account.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
