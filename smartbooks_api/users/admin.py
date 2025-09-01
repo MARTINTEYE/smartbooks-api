44e8164be80fcec295eb62f28ea394ab6c6b261b
Martin Teye <martinteye93@gmail.com>
2025-09-01 15:28:25 +0000

    added custom user model with date_of_birth and profile_photo

--- smartbooks_api/users/admin.py
+++ smartbooks_api/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
