77d1f34174caa22726ca1354a41bc5e8ca25db05
Martin Teye <martinteye93@gmail.com>
2025-09-01 15:32:49 +0000

    created a user/serializer.py

--- smartbooks_api/smartbooks_api/settings.py
+++ smartbooks_api/smartbooks_api/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'finance',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
